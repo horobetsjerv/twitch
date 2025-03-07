46f06986d1d153f6cb3d143a009a7fb5bec34aa1
Matvey <molodoytankist05@gmail.com>
2025-03-07 20:57:45 +0200

    try iix reg #2

--- twitch-nakrutka-backend/src/main.ts
+++ twitch-nakrutka-backend/src/main.ts
@@ -5,7 +5,7 @@ import { MainURL } from 'URLS';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials: true, origin: MainURL });
+  app.enableCors({ credentials: true, origin: [/debrabebra\.top$/] });
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 4444);
 }
