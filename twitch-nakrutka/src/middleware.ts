fdd6a3693e96c54819123f097bd23c6a11d875a7
Matvey <molodoytankist05@gmail.com>
2025-03-07 22:55:47 +0200

    try iix reg #35

--- twitch-nakrutka/src/middleware.ts
+++ twitch-nakrutka/src/middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
   const accessToken = request.cookies.get("access_token");
 
   if (!accessToken) {
-    return NextResponse.redirect(new URL("/auth", request.url));
+    // return NextResponse.redirect(new URL("/auth", request.url));
   }
 
   return NextResponse.next();
